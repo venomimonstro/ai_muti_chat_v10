b27a590cc22a6037676a52586ddfbc51ff102f3c
venomimonstro <lisencko.art@gmail.com>
2026-09-18 19:45:27 +0300

    feat: expose GitHub integration endpoints

---
+++ backend/apps/github_integration/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+
+from .views import (
+    GitHubCallbackView,
+    GitHubConnectView,
+    GitHubFileView,
+    GitHubInstallationListView,
+    GitHubProjectBindingView,
+    GitHubRepositoryListView,
+)
+
+urlpatterns = [
+    path("github/connect/", GitHubConnectView.as_view(), name="github-connect"),
+    path("github/callback/", GitHubCallbackView.as_view(), name="github-callback"),
+    path("github/installations/", GitHubInstallationListView.as_view(), name="github-installations"),
+    path(
+        "github/installations/<uuid:installation_id>/repositories/",
+        GitHubRepositoryListView.as_view(),
+        name="github-installation-repositories",
+    ),
+    path(
+        "projects/<uuid:project_id>/github/",
+        GitHubProjectBindingView.as_view(),
+        name="github-project-binding",
+    ),
+    path(
+        "projects/<uuid:project_id>/github/file/",
+        GitHubFileView.as_view(),
+        name="github-project-file",
+    ),
+]
