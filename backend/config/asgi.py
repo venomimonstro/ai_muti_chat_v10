6d73553da3fd3f4de01d50448a20090fe2845cc2
venomimonstro <lisencko.art@gmail.com>
2026-09-18 12:31:53 +0300

    fix: use runtime settings in ASGI

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -2,5 +2,5 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.runtime_settings")
 application = get_asgi_application()
