97e265b4d062f91f5f858e59092892ff11bc84a4
venomimonstro <lisencko.art@gmail.com>
2026-09-18 21:09:40 +0300

    feat: register billing loss watchdog signals

---
+++ backend/apps/billing/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class BillingConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.billing"
+
+    def ready(self):
+        from . import loss_watchdog  # noqa: F401
