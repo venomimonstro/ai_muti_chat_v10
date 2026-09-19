50e6251d664204338051b5d1d9428279e18949c9
venomimonstro <lisencko.art@gmail.com>
2026-09-19 11:45:33 +0300

    Merge conflicting image studio migrations

---
+++ backend/apps/image_studio/migrations/0004_merge_0003_imagegeneration_queued_state_0003_imagemodel_provider_price_matrix.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("image_studio", "0003_imagegeneration_queued_state"),
+        ("image_studio", "0003_imagemodel_provider_price_matrix"),
+    ]
+
+    operations = []
