f72c10a64dccdfd0e34423e6c25e56f06bf10f82
venomimonstro <lisencko.art@gmail.com>
2026-09-20 08:36:09 +0300

    Repair safe model activation before client catalog response

--- backend/apps/ai_registry/views.py
+++ backend/apps/ai_registry/views.py
@@ -1,6 +1,7 @@
 from rest_framework import mixins, viewsets
 
 from .models import AIModel
+from .reliability import ensure_safe_client_models
 from .serializers import AIModelSerializer
 
 
@@ -8,6 +9,7 @@ class AIModelViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = AIModelSerializer
 
     def get_queryset(self):
+        ensure_safe_client_models()
         return AIModel.objects.filter(enabled=True).select_related(
             "provider", "current_version"
         ).order_by("provider__priority", "display_name")
